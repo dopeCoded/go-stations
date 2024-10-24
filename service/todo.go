bf2cc61dece2d0ccd260e1669221001330348582
tanakaGit <100071732+dopeCoded@users.noreply.github.com>
2024-10-24 23:58:34 +0900

    after08beginner

--- service/todo.go
+++ service/todo.go
@@ -22,12 +22,46 @@ func NewTODOService(db *sql.DB) *TODOService {
 // CreateTODO creates a TODO on DB.
 func (s *TODOService) CreateTODO(ctx context.Context, subject, description string) (*model.TODO, error) {
 	const (
-		insert  = `INSERT INTO todos(subject, description) VALUES(?, ?)`
-		confirm = `SELECT subject, description, created_at, updated_at FROM todos WHERE id = ?`
+			insert  = `INSERT INTO todos(subject, description) VALUES(?, ?)`
+			confirm = `SELECT id, subject, description, created_at, updated_at FROM todos WHERE id = ?`
 	)
 
-	return nil, nil
+
+	// トランザクションの開始
+	tx, err := s.db.BeginTx(ctx, nil)
+	if err != nil {
+			return nil, err
+	}
+	defer tx.Rollback()
+
+	// TODO の挿入
+	result, err := tx.ExecContext(ctx, insert, subject, description)
+	if err != nil {
+			return nil, err
+	}
+
+	// 挿入したレコードの ID を取得
+	id, err := result.LastInsertId()
+	if err != nil {
+			return nil, err
+	}
+
+	// 挿入した TODO を取得
+	row := tx.QueryRowContext(ctx, confirm, id)
+	var todo model.TODO
+	if err := row.Scan(&todo.ID, &todo.Subject, &todo.Description, &todo.CreatedAt, &todo.UpdatedAt); err != nil {
+			return nil, err
+	}
+
+	// トランザクションのコミット
+	if err := tx.Commit(); err != nil {
+			return nil, err
+	}
+
+	// 作成した TODO を返す
+	return &todo, nil
 }
+
 
 // ReadTODO reads TODOs on DB.
 func (s *TODOService) ReadTODO(ctx context.Context, prevID, size int64) ([]*model.TODO, error) {
